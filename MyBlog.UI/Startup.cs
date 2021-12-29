5b226375d42bdf37c8e12d7530317e072b7bdbc3
Emre <94867996+EmreTomruk@users.noreply.github.com>
2021-12-29 10:14:52 +0300

    StartUp ChangesCreate README.md

--- MyBlog.UI/Startup.cs
+++ MyBlog.UI/Startup.cs
@@ -18,7 +18,9 @@ namespace ProgrammersBlog.Mvc
         {
 
         }
-
+        //hadi bakalým...
+        //Demet Akalin
+        //
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
