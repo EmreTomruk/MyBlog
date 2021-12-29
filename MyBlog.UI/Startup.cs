93a9b5badaeb17f651dcc1e0f697ec7b120a56f9
Emre <emretmrk39@gmail.com>
2021-12-29 11:24:15 +0300

    commit all

--- MyBlog.UI/Startup.cs
+++ MyBlog.UI/Startup.cs
@@ -18,9 +18,12 @@ namespace ProgrammersBlog.Mvc
         {
 
         }
-        //hadi bakalým...
+
+
+        //hadi bakalÄ±m...
         //Demet Akalin
         //
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
