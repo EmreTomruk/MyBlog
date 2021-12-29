609ac563058333e95e307b8f1dacd59652555fa0
Emre <emretmrk39@gmail.com>
2021-12-29 12:19:19 +0300

    new

--- MyBlog.UI/Startup.cs
+++ MyBlog.UI/Startup.cs
@@ -18,7 +18,7 @@ namespace ProgrammersBlog.Mvc
         {
 
         }
-
+        //yeni!!!
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
